7fba69ca9bf256c1e11bacf979c416aa5546ef63
aruuleon <arieleonchang@gmail.com>
2025-04-19 02:16:22 -0300

    implementacion para abrir ventana temporalmente

--- TP4_GRUPO_16/src/principal/PantallaPrincipal.java
+++ TP4_GRUPO_16/src/principal/PantallaPrincipal.java
@@ -1,8 +1,19 @@
 package principal;
 
+import java.awt.EventQueue;
+
 public class PantallaPrincipal {
 	
 	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+            try {
+                SeleccionMultiple seleccionMultiple = new SeleccionMultiple();
+                seleccionMultiple.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 		
 	}
 	
