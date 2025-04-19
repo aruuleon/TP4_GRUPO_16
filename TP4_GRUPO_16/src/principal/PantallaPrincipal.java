e17a39aff75fe6ae0afe7b4045ae9441aab30d52
tamicaa-mat <140745498+tamicaa-mat@users.noreply.github.com>
2025-04-19 13:46:38 -0300

    Update PantallaPrincipal.java

--- TP4_GRUPO_16/src/principal/PantallaPrincipal.java
+++ TP4_GRUPO_16/src/principal/PantallaPrincipal.java
@@ -3,7 +3,14 @@ package principal;
 public class PantallaPrincipal {
 	
 	public static void main(String[] args) {
-		
+		EventQueue.invokeLater(() -> {
+            try {
+                SeleccionMultiple seleccionMultiple = new SeleccionMultiple();
+                seleccionMultiple.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 	}
 	
 }
