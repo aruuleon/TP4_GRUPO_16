cfc334943bec37da238ca4ca950feb37499e0f37
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2025-04-20 06:37:43 -0300

    integracion de la clase VentanaEjercicio1 con sus variables y constructor seguimos..

--- TP4_GRUPO_16/src/principal/PantallaPrincipal.java
+++ TP4_GRUPO_16/src/principal/PantallaPrincipal.java
@@ -1,16 +1,40 @@
 package principal;
 
-public class PantallaPrincipalEjercicio1 {
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PantallaPrincipal extends JFrame {
 	
+	
+
+	
+
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
             try {
-                SeleccionMultiple seleccionMultiple = new SeleccionMultiple();
-                seleccionMultiple.setVisible(true);
+            	
+            	VentanaEjercicio1 frame = new VentanaEjercicio1() ;
+            	frame.setVisible(true);
+            	
+         
+               
+               
+               
             } catch (Exception e) {
                 e.printStackTrace();// ejercicio 1
             }
-        }
+        });
 	}
 	
+	
+	
+	
+	
+	
 }
+
---
+++ TP4_GRUPO_16/src/principal/VentanaEjercicio1.java
@@ -0,0 +1,29 @@
+package principal;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class VentanaEjercicio1 extends  JFrame {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+    private JTextField TextNombre;
+    private JTextField TextApellido;
+    private JTextField TextTelefono;
+    private JTextField TextFechaNacimiento;
+	
+	
+	public VentanaEjercicio1() {
+		
+		
+		// ACA ES DONDE SE VA A IMPLEMENTAR LA VENTANA DEL EJERCICIO 1 ..
+		
+		
+	}
+	
+	
+
+}
