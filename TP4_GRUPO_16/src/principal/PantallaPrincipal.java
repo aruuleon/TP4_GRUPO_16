feccce65402c928c883ccdc2749ca0efce6d5e03
aruuleon <arieleonchang@gmail.com>
2025-04-23 21:40:27 -0300

    implementacion pantalla principal

--- TP4_GRUPO_16/src/principal/PantallaPrincipal.java
+++ TP4_GRUPO_16/src/principal/PantallaPrincipal.java
@@ -1,8 +1,19 @@
 package principal;
 
+import java.awt.EventQueue;
+
 public class PantallaPrincipal {
 	
 	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+            try {
+                Principal principal = new Principal();
+                principal.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 		
 	}
 	
